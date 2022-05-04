4465571e7e97be3f1f50ffb0333dd5b82ab10804
meagancheese <meagan.konst@wustl.edu>
2022-05-04 18:27:56 -0500

    Removed docstring (should be in header).

--- RTS/targetfreq.cpp
+++ RTS/targetfreq.cpp
@@ -9,13 +9,7 @@
 #include <cmath>
 
 #include "targetfreq.h"
-/*
-* getTargetFreq takes in the actual frequency of the sound
-* sample in Hz and converts this frequency into a number
-* of semitones away from C0. This value is then rounded,
-* with the difference between the rounded and actual
-* values being returned in order to perform pitch correction.
-*/
+
 double getTargetFreq(double actualFreq) {
   // Frequency in Hz of A4
   double A4 = 440;
